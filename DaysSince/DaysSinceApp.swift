af3cb4a865f5df47247f05f865050ff2e06839a4
Richard Wise <richard.wise@hey.com>
2023-05-08 09:38:12 -0400

    Initial Commit

---
+++ DaysSince/DaysSinceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysSinceApp.swift
+//  DaysSince
+//
+//  Created by Richard Wise on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DaysSinceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
